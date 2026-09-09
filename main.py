8c3d6a03ff155f64b3742cfde211dfda1507e1ef
Victor <71899727+Victor-Krupeichenko@users.noreply.github.com>
2026-09-09 20:10:06 +0300

    Генерирует QR-код и создает .png файл с этим кодом (Generates a QR code and creates a .png file containing that code.)

---
+++ main.py
@@ -0,0 +1,51 @@
+import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
+
+
+class QrcodeGenerate:
+
+    def __init__(self, data):
+        """
+        :param data: Данные которые нужно записать в QR-код
+        """
+        self.data = data
+        self.qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_H,
+            box_size=10,
+            border=1
+        )
+        self.img = None
+
+    def add_data(self):
+        """
+        Добавляет данные и генерирует матрицу
+        """
+        self.qr.add_data(self.data)
+        self.qr.make(fit=True)
+
+    def changes_block(self):
+        """
+        Изменяет блок
+        """
+        self.img = self.qr.make_image(
+            image_factory=StyledPilImage,
+            eye_drawer=RoundedModuleDrawer(radius_ratio=1.0),
+            fill_color="black",
+            back_color="while"
+        )
+
+    def save_image(self):
+        """
+        Сохраняет картинку
+        """
+        self.img.save("Qr-code-Loev-tcson.png")
+
+    def main(self):
+        """
+        Основной метод(запускает генерацию QR-кода)
+        """
+        self.add_data()
+        self.changes_block()
+        self.save_image()
